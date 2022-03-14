c35249ae79a2ee39cf7ef6b830065b8b2f213a34
Yan <yanqizheng@YandeMacBook-Pro.local>
2022-03-15 01:33:10 +0800

    init

---
+++ src/main/java/dev/shawn/demo/DemoApiApplication.java
@@ -0,0 +1,15 @@
+package dev.shawn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class DemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiApplication.class, args);
+    }
+
+}
